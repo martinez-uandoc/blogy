fe5aaecebe74e0a940ea73287ee49f181ef570d2
CMartinezUAN <50330249+CMartinezUAN@users.noreply.github.com>
2025-06-21 13:56:38 -0700

    Initial commit

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,9 @@
+@extends("layouts.main")
+
+
+@section("titulo", "Mi sitio web de Laravel")
+
+
+@section("contenido")
+<h1 class="display-1 fw-bold text-white bg-dark">Hola desde una pagina con herencia</h1>
+@endsection
