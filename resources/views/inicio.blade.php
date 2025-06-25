0188c4c4e1ca91e6711973b594163ab2bb44c271
CMartinezUAN <50307863+JCMartinezR@users.noreply.github.com>
2025-06-24 17:01:30 -0700

    Update inicio.blade.php

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -5,12 +5,30 @@
 
 
 @section("contenido")
-<h1 class="display-1 fw-bold text-white bg-dark">Hola desde una pagina con herencia</h1>
+<ul class="list-group list-group-horizontal mt-4 justify-content-center fs-2">
+@foreach($etiquetas AS $e)
+<li class="list-group-item"s>
+    <a href="#">{{ $e->nombre }}</a>
+</li>
+@endforeach
+</ul>
 
-{{ $etiquetas }}
 <br>
 <br>
 <br>
 <br>
-{{ $articulos }}
+<div class="container d-flex flex-wrap justify-content-center">
+@foreach ($articulos as $a)
+<div class="card m-3" style="width: 18rem;">
+  <img src="{{ $a->portada }}" style="aspect-ratio: 1/1; object-fit:cover">
+  <div class="card-body">
+    <h5 class="card-title">{{ $a->titulo }}</h5>
+    <p class="card-text">{{ $a->descripcion }}</p>
+    <a href="#" class="btn btn-primary">📖 Ir a la nota</a>
+  </div>
+</div>
+@endforeach
+</div>
+
+
 @endsection
